2561e388ce60951cff6654d9be645cbed7b02839
h452yats <108774064+h452yats@users.noreply.github.com>
2022-07-13 11:06:35 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,11 @@
 #!/usr/bin/php
 <?php 
+$b='abc';
   foreach(file('/usr/share/dict/linux.words') as $line){
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+        $pos=strpos($line,$b);
+    if ($pos !== false){
+     echo $line;
+    }
+    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。 
   } 
 ?>
